ab5ebb561e297a79f8a828d930d967e5723bc02c
annie-82 <92351283+annie-82@users.noreply.github.com>
2021-10-31 00:56:49 -0500

    Update time-Lab 4.py

--- time-Lab 4.py
+++ time-Lab 4.py
@@ -4,6 +4,11 @@ waitTimeStr = input("How many hours do you want to wait") # Add a parenthesis si
 currentTimeInt = int(currentTimeStr) # Fix the name since it was a grammar mistake
 waitTimeInt = int(waitTimeStr) # Fix the name since it was a grammar mistake
 
+'''
+Logic error: finalTime_Int shows the time in hours 0-23.
+finalTime_Int = (currentTimeInt + waitTimeInt) % 24
+'''
+
 finalTime_Int = (currentTimeInt + waitTimeInt) # Missing the parenthesis
 # Also to fix the name for line 7 since it was missing a underscore
 print(finalTime_Int)
